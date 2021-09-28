46a4c0d42af09adb5483f21ca9fcaf81950e44c9
m05e5 <calebmoses1111@gmail.com>
2021-09-28 11:41:58 +0100

    add animal table with data queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,8 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+select * from animals where name like '%mon';
+select name from animals where date > '2016-01-01' AND date < '2019-12-30';
+select name from animals where neutered = true AND escape_attempts < 3;
+select date_of_birth from animals where name = 'Agumon' or name = 'Pikachu';
+select name, escape_attempts from animal where weight_kg > 10.5;
+
--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
+    id integer AUTO_INCREMENT
     name varchar(100)
+    date_of_birth date
+    escape_attempts integer
+    neutered boolean
+    weight_kg decimal
 );

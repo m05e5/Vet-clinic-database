64dc1f93fb42fe06d2ce05f251449d3d0fc33ab0
m05e5 <calebmoses1111@gmail.com>
2021-09-28 11:52:09 +0100

    final update

--- queries.sql
+++ queries.sql
@@ -5,4 +5,7 @@ select name from animals where date > '2016-01-01' AND date < '2019-12-30';
 select name from animals where neutered = true AND escape_attempts < 3;
 select date_of_birth from animals where name = 'Agumon' or name = 'Pikachu';
 select name, escape_attempts from animal where weight_kg > 10.5;
+select * from animals where neutered = true;
+select * from animals where name != 'Gabumon';
+select * from animals where weight_kg  BETWEEN 10.4 AND 17.3;
 

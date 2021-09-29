435f2ceecb15912b43205a3f4a1759472e1f99ad
m05e5 <calebmoses1111@gmail.com>
2021-09-29 12:13:07 +0100

    add data and queries

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id integer AUTO_INCREMENT PRIMARY KEy
+    id integer AUTO_INCREMENT PRIMARY KEY
     name varchar(100)
     date_of_birth date
     escape_attempts integer
     neutered boolean
     weight_kg decimal
+    species varchar(100) DEFAULT 
 );

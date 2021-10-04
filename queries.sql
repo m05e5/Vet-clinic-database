1f0ec0c3d94c2616f2412d28d20e9a7c6cea3eb9
m05e5 <calebmoses1111@gmail.com>
2021-10-04 13:30:24 +0100

    add a query

--- queries.sql
+++ queries.sql
@@ -124,3 +124,4 @@ ORDER BY count_species DESC LIMIT 1;
 
 SELECT COUNT(*) FROM visits where animal_id = 4;
 SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
